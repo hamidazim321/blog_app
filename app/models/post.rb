a7be631508b111457b966fb9f0f57e2aa8fbbbbb
hamid321 <hamidazim321@gmail.com>
2024-01-09 21:50:01 +0500

    add method for recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
   def update_post_counter
     user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments (limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
 end

c8ec9aeec58e57a282c7849876130ffc992806b3
hamid321 <hamidazim321@gmail.com>
2024-01-10 11:37:19 +0500

    Fix comments counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :update_comment_counter
+  after_save :update_comments_counter
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)

9c04ea535d5c0717316d6615d61d81f0870c706f
hamid321 <hamidazim321@gmail.com>
2024-01-11 15:07:49 +0500

    Add failing tests for Posts model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do 
+  let (:user) {User.create(name: 'Hamid', photo: 'PhotoURL', bio: "I'm a professional testing user")}
+  subject {Post.create(title: 'testing post', text: 'hello world', user: user)}
+
+  it 'is valid with a title' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without a title' do
+    subject.title = ""
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid if title exceeds 250 characters' do
+    invalid_title = 'a' * 251
+    subject.title = invalid_title
+    expect(subject.reload).not_to be_valid
+  end
+
+  it 'should associate the post with the correct author' do
+    expect(subject.user.name).to eq('Hamid') 
+  end
+
+  it 'is invalid if comments_counter is less than zero' do
+    subject.comments_counter = -1
+    expect(subject.reload).not_to be_valid
+  end
+
+  it 'is valid when comments_counter is positive integer' do
+    subject.comments_counter = 1
+    expect(subject.reload).to be_valid
+  end
+
+  it 'is invalid if likes_counter is less than zero' do
+    subject.comments_counter = -1
+    expect(subject.reload).not_to be_valid
+  end
+
+  it 'is valid when likes_counter is positive integer' do
+    subject.comments_counter = 1
+    expect(subject.reload).to be_valid
+  end
+end

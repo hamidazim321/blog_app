ede1549e9dea2d4a558f95d5e72f3754c4efbc67
hamid321 <hamidazim321@gmail.com>
2024-01-16 14:40:20 +0500

    Add controller specs for Posts

--- Gemfile
+++ Gemfile
@@ -46,17 +46,11 @@ gem 'bootsnap', require: false
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
-  # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
-
-  # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
-  # gem "rack-mini-profiler"
-
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
 end
 
 group :test do
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let (:user) {User.create(name: 'Hamid', id: 1)}
+  let (:user_url) {user_url(user)}
+
+  def create_post(author)
+    Post.create(
+      title: 'Test post',
+      text: 'This is for controller testing',
+      user: author
+    )
+  end
+
+  describe 'Get /index' do
+    before do
+      user_post = create_post(user)
+      get user_posts_path(user)
+    end
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'The response status is 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template("posts/index")
+    end
+
+    it 'template includes the correct body' do
+      expect(response.body).to include('<h1>Specific user posts goes here</h1>')
+    end
+  end
+
+  describe 'Get /show' do
+    before do
+      user_post = create_post(user)
+      get user_post_path(user, user_post)
+    end
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'The response status is 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template("posts/show")
+    end
+
+    it 'template includes the correct body' do
+      expect(response.body).to include('<h1>Specific post of a user goes here</h1>')
+    end
+  end
 end

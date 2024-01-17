d6f221c290551bfcb3aa6548412cda12c2b41a3b
hamid321 <hamidazim321@gmail.com>
2024-01-17 13:37:54 +0500

    Add view for users/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   before_validation :set_comments_counter, :set_likes_counter
   after_save :update_posts_counter

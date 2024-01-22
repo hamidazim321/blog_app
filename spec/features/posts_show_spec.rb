e5aef58aca4098aa6adea9b60e62db9daab18a78
hamid321 <hamidazim321@gmail.com>
2024-01-22 20:49:20 +0500

    Fix tests for posts show

--- spec/features/posts_show_spec.rb
+++ spec/features/posts_show_spec.rb
@@ -1,66 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :feature do
-  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+
+  before :each do
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post = Post.create(user: @user, title: 'Hello', text: 'This is my first post')
+    @comment = Comment.create(text: 'test comment', user: @user, post: @post)
+  end
   context 'show page' do
     it "shows the post's title" do
-      if user.posts.any?
-        first_recent_post = user.recent_posts(1)
-        visit user_post_path(user, first_recent_post)
-        expect(page).to have_content(first_recent_post.title)
-      end
+      recent_post = @user.posts[0]
+      visit user_post_path(@user, recent_post)
+      expect(page).to have_content(recent_post.title)
     end
     it "shows the post's author" do
-      if user.posts.any?
-        first_recent_post = user.recent_posts(1)
-        visit user_post_path(user, first_recent_post)
-        expect(page).to have_content(first_recent_post.user.name)
-      end
+      recent_post = @user.posts[0]
+      visit user_post_path(@user, recent_post)
+      expect(page).to have_content(recent_post.user.name)
     end
     it 'shows how many likes the post has.' do
-      if user.posts.any?
-        first_recent_post = user.recent_posts(1)
-        visit user_post_path(user, first_recent_post)
-        expect(page).to have_content("Likes: #{first_recent_post.likes_counter}")
-      end
+      recent_post = @user.posts[0]
+      visit user_post_path(@user, recent_post)
+      expect(page).to have_content("Likes: #{recent_post.likes_counter}")
     end
     it 'shows how many comments the post has.' do
-      if user.posts.any?
-        first_recent_post = user.recent_posts(1)
-        visit user_post_path(user, first_recent_post)
-        expect(page).to have_content("Comments: #{first_recent_post.comments_counter}")
-      end
+      recent_post = @user.posts[0]
+      visit user_post_path(@user, recent_post)
+      expect(page).to have_content("Comments: #{recent_post.comments_counter}")
     end
   end
 
   it 'shows how the post body.' do
-    if user.posts.any?
-      first_recent_post = user.recent_posts(1)
-      visit user_post_path(user, first_recent_post)
-      expect(page).to have_content(first_recent_post.text)
-    end
+    recent_post = @user.posts[0]
+    visit user_post_path(@user, recent_post)
+    expect(page).to have_content(recent_post.text)
   end
   it 'shows how the username of each commentor' do
-    if user.posts.any?
-      first_recent_post = user.recent_posts(1)
-      visit user_post_path(user, first_recent_post)
-      if first_recent_post.comments.any?
-        first_recent_post.comments.each do |comment|
-          expect(page).to have_content(comment.user.name)
-        end
-      end
+    recent_post = @user.posts[0]
+    visit user_post_path(@user, recent_post)
+    recent_post.comments.each do |comment|
+      expect(page).to have_content(comment.user.name)
     end
   end
   it 'shows the comment each commentor left.' do
-    if user.posts.any?
-      first_recent_post = user.recent_posts(1)
-      visit user_post_path(user, first_recent_post)
-      if first_recent_post.comments.any?
-        first_recent_post.comments.each do |comment|
-          expect(page).to have_content(comment.text)
-        end
-      end
+    recent_post = @user.posts[0]
+    visit user_post_path(@user, recent_post)
+    recent_post.comments.each do |comment|
+      expect(page).to have_content(comment.text)
     end
   end
 end

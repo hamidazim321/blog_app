ed1bf95f5db04e5922f7f9abdba782271f5c6e0a
hamid321 <hamidazim321@gmail.com>
2024-01-22 18:18:43 +0500

    Eager load comments in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts).find(params[:user_id])
+    @user = User.includes(:posts, :comments).find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
-    @user = User.includes(:posts).find(params[:user_id])
+    @user = User.includes(:posts, :comments).find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
 

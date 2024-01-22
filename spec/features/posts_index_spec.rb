234dd9a9e9f97c1d286e7f2472fa738e1dbced6d
hamid321 <hamidazim321@gmail.com>
2024-01-22 20:38:57 +0500

    Fix tests for posts index

--- spec/features/posts_index_spec.rb
+++ spec/features/posts_index_spec.rb
@@ -1,32 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :feature do
-  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  let(:post) { Post.create(user:, title: 'Hello', text: 'This is my first post') }
+
+  before :each do
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post =  Post.create(user: @user, title: 'Hello', text: 'This is my first post')
+  end
+  
   context 'index page' do
     it "show user's username" do
-      visit user_posts_path(user)
-      expect(page).to have_content(user.name)
+      visit user_posts_path(@user)
+      expect(page).to have_content(@user.name)
     end
 
     it 'show user profile picture' do
-      visit user_posts_path(user)
-      expect(page).to have_selector("img[src='#{user.photo}']")
+      visit user_posts_path(@user)
+      expect(page).to have_selector("img[src='#{@user.photo}']")
     end
 
     it 'shows the number of posts user has' do
-      visit user_posts_path(user)
-      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+      visit user_posts_path(@user)
+      expect(page).to have_content("Number of posts: #{@user.posts_counter}")
     end
     it "shows a post's title" do
-      visit user_posts_path(user)
-      recent_post = user.recent_posts[0]
+      visit user_posts_path(@user)
+      recent_post = @user.posts[0]
       expect(page).to have_content(recent_post.title) if recent_post
     end
 
     it "shows some of the post's body." do
-      visit user_posts_path(user)
-      recent_post = user.recent_posts[0]
+      visit user_posts_path(@user)
+      recent_post = @user.posts[0]
       if recent_post
         if recent_post.text.length > 50
           expect(page).to have_content("#{recent_post.text[0, 49]}...")
@@ -38,11 +42,11 @@ RSpec.describe 'Post', type: :feature do
   end
   context 'Click' do
     it "redirects me to that post's show page when I click on a post" do
-      visit user_posts_path(user)
-      if user.posts.any?
-        recent_post = user.recent_posts[0]
+      visit user_posts_path(@user)
+      if @user.posts.any?
+        recent_post = @user.posts[0]
         click_link recent_post.title
-        expect(page).to have_current_path(user_post_path(user, recent_post))
+        expect(page).to have_current_path(user_post_path(@user, recent_post))
       end
     end
   end

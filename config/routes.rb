ec7e9950349ee993e1039931fab4a8609ed4ef85
hamid321 <hamidazim321@gmail.com>
2024-01-23 19:34:04 +0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]

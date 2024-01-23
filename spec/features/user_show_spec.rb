4cbe7a494d78bc06d2a63ec29c37f766e616e5a0
hamid321 <hamidazim321@gmail.com>
2024-01-23 13:00:47 +0500

    Fix linter errors

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.feature 'User Show Page', type: :feature do
   before do
     @user = User.create(name: 'example_user', bio: 'This is a bio', photo: 'https://unsplash.com/photos/F_-0BxGuVvo')
-    @post1 = @user.posts.create(title: 'Post 1', text: 'Content for post 1') 
-    @post2 = @user.posts.create(title: 'Post 2', text: 'Content for post 2') 
-    @post3 = @user.posts.create(title: 'Post 3', text: 'Content for post 3') 
+    @post1 = @user.posts.create(title: 'Post 1', text: 'Content for post 1')
+    @post2 = @user.posts.create(title: 'Post 2', text: 'Content for post 2')
+    @post3 = @user.posts.create(title: 'Post 3', text: 'Content for post 3')
 
     visit user_path(@user)
   end
 
   scenario 'Display user information' do
     expect(page).to have_selector("img[src='#{@user.photo}']")
-    expect(page).to have_content(@user.name) 
-    expect(page).to have_content('Number of posts: 3') 
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content('Number of posts: 3')
     expect(page).to have_content(@user.bio)
   end
 

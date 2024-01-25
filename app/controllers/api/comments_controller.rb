13ca69c41fe70bd7307cd8522548cc080e717f4f
hamid321 <hamidazim321@gmail.com>
2024-01-25 16:02:37 +0500

    Add controller action to post comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,7 +1,25 @@
 class Api::CommentsController < Api::ApiController
+  
   def index
     @post = Post.includes(:comments).find_by_id(params[:post_id])
     @comments = @post.comments
     render json: @comments
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = @post
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { success: false, errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
